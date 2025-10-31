74053c65aeca1a07b69a41957d92699089a51ccb
Denis Arruda Santos <denis.arruda@gmail.com>
2025-10-30 21:27:34 -0300

    Adding prompt template, User prompt and system prompt.

--- src/main/java/com/denisarruda/dad_jokes/ChatController.java
+++ src/main/java/com/denisarruda/dad_jokes/ChatController.java
@@ -2,6 +2,9 @@ package com.denisarruda.dad_jokes;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,5 +25,14 @@ public class ChatController {
             .call()
             .content();
     }
-    
+
+    @GetMapping("/jokes")
+    public String jokes(@RequestParam(value = "message", defaultValue = "Tell me a dad joke") String message) {
+        var system = new SystemMessage("Your primary function is to tell dad jokes. If someone asks you for any other type of joke tell them you only know dad jokes.");
+        var user = new UserMessage(message);
+        Prompt prompt = new Prompt(system, user);
+        return this.chatClient.prompt(prompt)
+            .call()
+            .content();
+    }
 }
---
+++ src/main/java/com/denisarruda/dad_jokes/YouTubeController.java
@@ -0,0 +1,46 @@
+package com.denisarruda.dad_jokes;
+
+import java.util.Map;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/youtube")
+public class YouTubeController {
+
+    private final ChatClient chatClient;
+
+    @Value("classpath:prompts/music.st")
+    private Resource musicPrompt;
+
+    public YouTubeController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+    
+    @GetMapping("popular")   
+    public String findPopularYoutubersByGenre(@RequestParam(value = "genre", defaultValue = "tech") String genre) {
+        String message = """
+                List 10 of most popular YouTubers in {genre} along with their subscriber counts. If you don't know the 
+                answer, just say 'I don't know'.
+                """;
+        PromptTemplate promptTemplate = new PromptTemplate(message);
+        Prompt prompt = promptTemplate.create(Map.of("genre", genre));
+        return chatClient.prompt(prompt).call().content();
+    }
+
+    @GetMapping("music")   
+    public String findPopularMusicsByGenre(@RequestParam(value = "genre", defaultValue = "rock") String genre) {
+        PromptTemplate promptTemplate = new PromptTemplate(musicPrompt);
+        Prompt prompt = promptTemplate.create(Map.of("genre", genre));
+        return chatClient.prompt(prompt).call().content();
+    }
+}
